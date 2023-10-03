fc26520903c5cbbde4d529ad22c34551bdad4531
YulinarNR <yulinarnurrahma@gmail.com>
2023-10-03 11:16:16 +0700

    Latihan: Stream

---
+++ stream/index.js
@@ -0,0 +1,34 @@
+const { error } = require("console");
+const fs = require("fs");
+const path = require("path");
+
+// path atau lokasi file
+const inputPath = path.resolve(__dirname, "input.txt");
+const outputPath = path.resolve(__dirname, "output.txt");
+
+// Membuat readable stream dengan highWaterMark sebesar 15 bytes
+const readableStream = fs.createReadStream(inputPath, {
+  encoding: "UTF-8",
+  highWaterMark: 15,
+});
+
+// membuat writable stream ouput.txt
+const writableStream = fs.createWriteStream(outputPath);
+
+readableStream.on("data", (txtInput) => {
+  // Menampilkan txtInput ke konsol
+  console.log(txtInput);
+  writableStream.write(txtInput + "\n");
+});
+
+readableStream.on("end", () => {
+  console.log("Done");
+});
+
+readableStream.on("error", (error) => {
+  console.error("Terjadi kesalahan saat membaca file:", error);
+});
+
+writableStream.on("error", (error) => {
+  console.error("Terjadi kesalahan saat menulis ke file:", error);
+});
